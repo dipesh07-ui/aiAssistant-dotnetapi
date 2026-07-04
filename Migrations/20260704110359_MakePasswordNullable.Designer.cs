c8282d7eefaff385fd8b35e64567b2dd0a205df2
dipesh07-ui <guptadipesh185@gmail.com>
2026-07-04 16:39:48 +0530

    fix:IPasswaordhasher null for google user

--- Migrations/20260704110359_MakePasswordNullable.Designer.cs
+++ Migrations/20260704110359_MakePasswordNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aiAssistant.api.Data;
 
@@ -11,9 +12,11 @@ using aiAssistant.api.Data;
 namespace aiAssistant.api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260704110359_MakePasswordNullable")]
+    partial class MakePasswordNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string? PasswordHash { get; set; }
         public string? Name { get; set; }
         public string? GoogleId { get; set; }
         public DateTime CreatedAt { get; set; }
